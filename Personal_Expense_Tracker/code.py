533d69f014b58ec1334338c5fa24d2ff00f43620
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-02-11 16:33:36 +0530

    Update code.py

--- Personal_Expense_Tracker/code.py
+++ Personal_Expense_Tracker/code.py
@@ -1,1 +1,19 @@
 
+"""PERSONAL EXPENSE TRACKER"""
+# A personal expense management system with categories, budgets and analytics
+
+import json
+import os
+from datetime import datetime
+from collections import defaultdict
+
+
+class expenseTracker:
+    def __init__(self, filaname = "expenses.json"):
+        self.filename = filename
+        self.expenses = []
+        self.categories = ["Food", "Entertainment", "Health", "Other", "Bills", "Shopping", "Transport"]
+        self.budget_limits = {}
+        self.load_date()
+        
+         
